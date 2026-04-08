1798a50dabfcee5ad6a2ed0e52eb7c39ebb1bbec
Heveraldo Augusto <heveraldoa8@gmail.com>
2026-04-08 15:26:44 -0300

    Commit inicial

---
+++ src/main/java/com/example/EcoFlux/EcoFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcoFlux/EcoFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
